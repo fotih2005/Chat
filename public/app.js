a387fc08c74091c2ddfe6d5856d5eae0384239bc
fotih2005 <fotihyusupov04@gmail.com>
2022-10-08 17:34:23 +0500

    app.js

--- public/app.js
+++ public/app.js
@@ -2,9 +2,11 @@ const socket = io();
 const users = document.querySelector(".name");
 
 const name = prompt("Ismingizni kiriting");
+localStorage.setItem('name', JSON.stringify(name))
+
 
 let h3 = document.createElement("h3")
-h3.textContent = textContent = "You Joined";
+h3.textContent = "You Joined";
 users.appendChild(h3);
 
 socket.emit("new-user", name);
@@ -18,19 +20,18 @@ socket.on("new-user-joined", (newUser) => {
 
 btn.addEventListener('click', () => {
     let p = document.createElement("p")
-    p.textContent = textContent = `You: ${message.value}`;
+    p.textContent = `You: ${message.value}`;
     users.appendChild(p);
-    socket.emit('new-message', {message: message.value})
+    socket.emit('new-message', {sender: name,    message: message.value})
 })
 
 socket.on('new-user-message', ({sender, message}) => {
     let p = document.createElement("p")
-    p.textContent = textContent = `${sender}: ${message}`;
+    p.textContent = `${sender}: ${message}`;
     users.appendChild(p);
-    socket.emit('new-message', {message: message.value})
 })
 
-message.addEventListener('keyup', e => {
+message.addEventListener('keyup', () => {
     socket.emit('user-typing', {sender: name})
 })
 
